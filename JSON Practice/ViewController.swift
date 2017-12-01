6afe7cffebf537f1e40caeac3869e1d413680e9b
Kelly Douglass <kdouglass@gmail.com>
2017-12-01 10:05:19 -0600

    Simple working version. gets json from web and shows data in table view list

--- JSON Practice/ViewController.swift
+++ JSON Practice/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	@IBOutlet weak var table: UITableView!
+	var productsArr : JSON?
+	
+	
+	let url = "https://metaderm.herokuapp.com/products"
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if productsArr != nil {
+			return productsArr!.count
+		} else {
+			return 1
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+		
+		if let productsReturned = productsArr {
+			cell.textLabel?.text = String(describing: productsReturned[indexPath.row]["productName"])
+		}
+		return cell
+		
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		Alamofire.request(url).responseJSON{
+			response in
+			if response.result.isSuccess {
+				print("Success")
+				let data : JSON = JSON(response.result.value!)
+				
+				self.productsArr = data
+				self.table.reloadData()
+			} else {
+				print("Error")
+			}
+		}
+		
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +59,9 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	override func viewDidAppear(_ animated: Bool) {
+		table.reloadData()
+	}
 
 }
 
