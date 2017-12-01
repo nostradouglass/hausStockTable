78fdd79f0512c02661fb51f89f0b65b2097c7de7
Kelly Douglass <kdouglass@gmail.com>
2017-11-30 22:57:32 -0600

    Initial Commit

---
+++ JSON Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON Practice
+//
+//  Created by Kelly Douglass on 11/30/17.
+//  Copyright © 2017 Kelly Douglass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
